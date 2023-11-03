4b6bc0b2264bdbd808f1617b0ea78359db46d397
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-11-03 16:41:32 -0500

    Added connection.js file in server direct... Added additional files that will be needed

---
+++ server/server.js
@@ -0,0 +1,6 @@
+// const cors = require('cors');
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const { ApolloServer, gql } = require('apollo-server-express');
+// const stripe = require('stripe')(process.env.STRIPE_KEY);
+
