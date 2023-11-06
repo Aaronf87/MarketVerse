f7be1107a465a156da9625c3484fac333554b3cc
Aaron Flores <aaron_flores08@yahoo.com>
2023-11-06 12:59:23 -0600

    added header, footer, routes to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,20 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<Login/>}/>
+      <Route path="/contact" element={<Register/>}/>
+      <Route path="*" element={<NotFound/>}/>
+    </Routes>
+    <Footer/>
+    </>
+
+
+
+
     </>
   )
 }
