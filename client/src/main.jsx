356dd1e6c23b791aecd73479c5704769aa26bd95
Aaron Flores <aaron_flores08@yahoo.com>
2023-11-06 17:21:20 -0600

    created outlet and createbrowser router with home path

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
 import 'tailwindcss/tailwind.css';
 import './styles/index.css'
+import Home from './pages/Home.jsx';
+import NotFound from './pages/notfound.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
