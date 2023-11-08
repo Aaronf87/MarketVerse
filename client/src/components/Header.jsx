a10c424c8f3b568a3a3b348e163e831b1cc1fc6b
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-11-07 20:34:14 -0600

    Created Header and Nav components

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 export default function Header() {
     return (
-        <header>
-
+        <header className="sticky top-0 z-[20] mx-auto flex w-full items-center justify-between border-b border-gray-300 p-5 shadow-lg">
+        <Nav />
         </header>
-    )
-}
+    );
+};
+
+export default Header;
---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+
+const Nav= () => {
+    return (
+        <>
+        <NavLink to="">STORE</NavLink>
+        <NavLink to="">Login</NavLink>
+        <NavLink to="">SING UP</NavLink>
+        </>
+    );
+}
+
+export default Nav;
