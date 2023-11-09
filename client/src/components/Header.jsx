07ff9ae5ee6bdf50ef7c7b142b12d1f2c070ef97
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-11-08 19:05:37 -0600

    Fixed small bug

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,3 +1,5 @@
+import Nav from './Nav';
+
 export default function Header() {
     return (
         <header className="sticky top-0 z-[20] mx-auto flex w-full items-center justify-between border-b border-gray-300 p-5 shadow-lg">
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,3 @@
 export default function Home() {
-    return (
-     
-          <h1 >
-      Home Page here!
-          </h1>
-
-    );
-  }
+  return <h1>Home Page here!</h1>;
+}
