58903bc783c1554bb903b0a049d5494bbfd9e105
Aaron Flores <aaron_flores08@yahoo.com>
2023-11-09 22:46:00 -0600

    flex items on allcategories

--- client/src/components/UI/CategoryList.jsx
+++ client/src/components/UI/CategoryList.jsx
@@ -2,22 +2,22 @@ export default function CategoryList({ categoryData }) {
   return (
     <>
       {categoryData.map((category) => (
-        <div key={category._id} className=" p-10">
-          <a className="card" href="">
+        <div key={category._id} className=" p-10 card">
+         
             <div>
               <div>
                 <dt className="text-3xl p-3">
                   {category.name}
                 </dt>
-                <img
+                {/* <img
                   className="rounded"
                   src={`https://source.unsplash.com/random/384x512?sig=${Math.random()}
                   `}
                   alt="hero image"
-                />
+                /> */}
               </div>
             </div>
-          </a>
+   
         </div>
       ))}
     </>
