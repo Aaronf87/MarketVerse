3550c8536f1b678f3128b7a8943e9a284fd9a120
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-11-11 12:17:13 -0600

    Made Logo a Link to Home

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,8 +7,10 @@ const Header = () => {
     return (
         <header className="relative flex w-full flex-wrap items-center justify-between bg-[#FBFBFB] py-5 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:py-4">
             <div className="flex w-full flex-wrap items-center justify-between px-3">
-                  {/* Logo */}
-                <Logo className="ml-2 text-xl text-neutral-800 dark:text-neutral-200" href=""/>
+                <Link to="/">
+                    {/* Logo */}
+                    <Logo className="ml-2 text-xl text-neutral-800 dark:text-neutral-200" href="" />
+                </Link>
 
                 <div className="flex items-center text ">
 
