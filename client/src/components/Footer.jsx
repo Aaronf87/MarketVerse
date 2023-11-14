10ab03b3467e5db5269a91d938c52b97588c97db
keybit-1 <131838974+keybit-1@users.noreply.github.com>
2023-11-13 20:16:07 -0600

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,13 @@
 export default function Footer() {
     return (
-        // <!-- Footer -->
         <footer className="bg-neutral-100 text-center text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200">
             {/* <!-- Social media --> */}
             <div className="flex items-center justify-center border-b-2 border-neutral-200 pb-2 dark:border-neutral-500 lg:justify-between">
                 <div className="mr-12 hidden md:block">
-                    <span>Get connected with us:</span>
+                <span>Get connected with us:</span>
+                </div>
+                <div className="flex justify-center items-center">
+                {/* <span>Get connected with us:</span> */}
                 </div>
                 <div className="flex justify-center items-center">
                     <a href="https://github.com/Aaronf87/MarketVerse" className="text-neutral-600 dark:text-neutral-200">
@@ -18,18 +20,43 @@ export default function Footer() {
                             <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
                         </svg>
                     </a>
+                    {/* Add more social media links here */}
+                    <a href="https://linkedin.com/in/your-linkedin-profile" className="text-neutral-600 dark:text-neutral-200">
+                        {/* LinkedIn icon */}
+                    </a>
+                    <a href="https://twitter.com/your-twitter-handle" className="text-neutral-600 dark:text-neutral-200">
+                        {/* Twitter icon */}
+                    </a>
+                    {/* Add more as needed */}
                 </div>
             </div>
 
-            {/* <!-- Main container --> */}
+            {/* <!-- About Us --> */}
             <div className="mx-6 pt-4 text-center">
-                {/* <!-- Text --> */}
                 <div className="mb-6">
-                    <h5 className="mb-2 font-bold uppercase">Explore Our World</h5>
+                    <h5 className="mb-2 font-bold uppercase">About Us</h5>
+                    <p>We are a team of passionate individuals working together to bring you the best marketplace experience.</p>
+                </div>
+            </div>
 
-                    <p className="mb-4">
-                        Text here blah blah blah
-                    </p>
+            {/* <!-- Contact information --> */}
+            <div className="mx-6 pt-4 text-center">
+                <div className="mb-6">
+                    <h5 className="mb-2 font-bold uppercase">Contact Us</h5>
+                    <p>Email: your-email@example.com</p>
+                    <p>Phone: (123) 456-7890</p>
+                    {/* Add more as needed */}
+                </div>
+            </div>
+
+            {/* <!-- Newsletter subscription --> */}
+            <div className="mx-6 pt-4 text-center">
+                <div className="mb-6">
+                    <h5 className="mb-2 font-bold uppercase">Subscribe to Our Newsletter</h5>
+                    <form>
+                        <input type="email" placeholder="Enter your email" />
+                        <button type="submit">Subscribe</button>
+                    </form>
                 </div>
             </div>
 
