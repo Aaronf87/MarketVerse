5e4db1df4dc981cf82c9cb6bbf81cfbb215ee065
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-11-14 22:13:44 -0600

    Update Hero.jsx
    Change hero jsx and css

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,42 +1,53 @@
-import hero from "/src/assets/blue.png";
-
-export default function Hero() {
-  const style = {
-    backgroundImage: `url(${hero})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    height: "50vh",
-    color: "white",
-    // padding: "25px 90px 90px 1100px",
+import hero from "../assets/shop.png";
+const Hero = () => {
+  const heroContainerStyle = {
+    position: 'relative',
+    textAlign: 'left', // Adjust as needed
+    color: '#fff', // Text color
   };
+
+  const heroImageStyle = {
+    width: '100%', // Adjust as needed
+    height: '500px',
+    display: 'block',
+  };
+
+  const heroTextStyle = {
+    position: 'absolute',
+    top: '50%',
+    right: '10%', // Adjust as needed
+    transform: 'translate(0%, -50%)',
+  };
+
+  const headingStyle = {
+    fontSize: '6em', // Adjust as needed
+    margin: 0,
+  };
+
+  const paragraphStyle = {
+    fontSize: '1.5em', // Adjust as needed
+    margin: '10px 0 0',
+    textAlign: 'center',
+  };
+
   const hr = {
-    width: "90%",
-    height: "3px",
+    width: "100%",
+    height: "5px",
     margin: "auto",
   };
 
   return (
-    <>
-      <section className="bg-gray-50 " style={style}>
-        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-          <div className="mx-auto max-w-xl text-center">
-            <div className="font-extrabold text-3xl sm:block">
-              Welcome to <br className="xl:hidden" />
-              <h1> MarketVerse </h1>
-              <hr style={hr} />
-              
-            </div>
-
-            <p className="mt-3 sm:text-xl/relaxed">
-            Your Online Store where <br></br> you      
-              Buy and Sell your way
-      </p> 
-
-            <div className="mt-8 flex flex-wrap justify-center gap-4"></div>
-          </div>
-        </div>
-      </section>
-    </>
+    <div style={heroContainerStyle}>
+      <img src={hero} alt="Hero Image" style={heroImageStyle} />
+      <div style={heroTextStyle}>
+        <h1 style={headingStyle}>MarketVerse</h1>
+        <hr style={hr}></hr>
+        <p style={paragraphStyle}>Your Online Store where <br></br>
+        You buy and sell your way
+        </p>
+      </div>
+    </div>
   );
-}
+};
+
+export default Hero;
